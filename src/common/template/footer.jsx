1a2fb5562dd891aa3595057e15e8d316c1097512
Marcelo Kath <marcelokathmk@gmail.com>
2018-10-25 09:42:57 -0300

    Criação do componente rodapé.

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright 2018
+            <a href='http://google.com.br' target='_blank'>Link Empresa</a>
+        </strong>
+    </footer>
+)
