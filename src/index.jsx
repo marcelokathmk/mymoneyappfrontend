29ae4d218b98a1574f3abf314ef4640177b8bc0e
Marcelo Kath <marcelokathmk@gmail.com>
2019-01-06 20:50:38 -0200

    Chamada de serviço para valores e criação de reducer para dashboard.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './main/reducers'
 import App from './main/app'
+import promise from 'redux-promise'
 
-ReactDom.render(<App />, document.getElementById('app'))
+const store = applyMiddleware(promise)(createStore)(reducers)
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('app'))
